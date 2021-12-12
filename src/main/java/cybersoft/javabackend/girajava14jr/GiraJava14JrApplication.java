1a824160cc872bf8418b2554031573a430bc256f
Leo <89365459+namtran158@users.noreply.github.com>
2021-12-12 09:47:41 +0700

    Initial commit

---
+++ src/main/java/cybersoft/javabackend/girajava14jr/GiraJava14JrApplication.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.girajava14jr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraJava14JrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiraJava14JrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cybersoft/javabackend/girajava14jr/GiraJava14JrApplicationTests.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.girajava14jr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraJava14JrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
